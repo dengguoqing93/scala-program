ae9d90a2d7870c19f68aac9ba28441de2eb71a6b
dengguoqing <dengguoqing93@163.com>
2018-11-29 18:00:31 +0800

    scala编程第三版（函数）

---
+++ src/main/scala/trait/Frog.scala
@@ -0,0 +1,11 @@
+package `trait`
+
+/**
+  * $DESCRIPTION
+  *
+  * @author DGQ
+  * @since 2018/11/28
+  */
+class Frog extends Philosophical {
+  override def toString: String = "green"
+}
---
+++ src/main/scala/trait/Philosophical.scala
@@ -0,0 +1,13 @@
+package `trait`
+
+/**
+  * $DESCRIPTION
+  *
+  * @author DGQ
+  * @since 2018/11/28
+  */
+trait Philosophical {
+  def philosophize()={
+    println("I consume memory,therefore I am!")
+  }
+}
